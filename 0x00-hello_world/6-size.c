5949482684471767c72e44a9b0b1b997f85f6749
Emmanuel <shnji.ogami@gmail.com>
2019-09-18 22:33:53 +0000

    Fix the point 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -11,10 +10,9 @@ char a;
 int b;
 long int c;
 float d;
-  
 printf("Size of a: %lu.\n", (ussigned long)size(a));
 printf("Size of an: %lu.\n", (ussigned long)size(b));
 printf("Size of a: %lu.\n", (ussigned long)size(c));
 printf("Size of a: %lu.\n", (ussigned long)size(d));
-return (0)
+return (0);
 }
