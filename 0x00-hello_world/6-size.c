73c769e4297ae215078a777855f8280e157ccb0b
Emmanuel <shnji.ogami@gmail.com>
2019-09-18 22:23:55 +0000

    Add point 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+float d;
+  
+printf("Size of a: %lu.\n", (ussigned long)size(a));
+printf("Size of an: %lu.\n", (ussigned long)size(b));
+printf("Size of a: %lu.\n", (ussigned long)size(c));
+printf("Size of a: %lu.\n", (ussigned long)size(d));
+return (0)
+}
