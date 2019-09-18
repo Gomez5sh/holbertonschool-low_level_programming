809b9fb79c2052ed7cbbdda79bd55997790c268b
Emmanuel <shnji.ogami@gmail.com>
2019-09-18 22:37:34 +0000

    Fix the 6 point again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,12 @@ int main(void)
 char a;
 int b;
 long int c;
-float d;
+long long int d;
+float e;
 printf("Size of a: %lu.\n", (ussigned long)size(a));
 printf("Size of an: %lu.\n", (ussigned long)size(b));
 printf("Size of a: %lu.\n", (ussigned long)size(c));
 printf("Size of a: %lu.\n", (ussigned long)size(d));
+printf("Size of a: %lu.\n", (ussigned long)size(e));
 return (0);
 }
