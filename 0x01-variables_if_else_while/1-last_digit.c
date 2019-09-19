0465ed0925854917265b078fb9e91b148ec9aa37
Emmanuel <shnji.ogami@gmail.com>
2019-09-19 22:16:07 +0000

    Erase the second var of point 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,21 @@
 int main(void)
 {
 	int n;
-	int l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10
 
-	if (l < 5)
+	if (n % 10 < 5)
 	{
-		printf("Last digit of %d %dand is greater than 5\n", n, l);
+		printf("Last digit of %d and is greater than 5\n", n);
 	}
-	if (l == 0)
+	if (n % 10 == 0)
 	{
-		printf("Last digit of %d %d and is 0\n", n, l);
+		printf("Last digit of %d and is 0\n", n);
 	}
-	if (l > 6)
+	if (n % 10 > 6)
 	{
-		printf("Last digit of %d %d and is less than 6 and not 0\n", n, l);
+		printf("Last digit of %d and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
