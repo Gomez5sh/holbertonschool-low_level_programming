eb452e4ef52528cc3aaa8b1bd6cd0d5e0ccd3f64
Emmanuel <shnji.ogami@gmail.com>
2019-09-19 18:14:56 +0000

    Fix the positive and negative program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+#define RAND_MAX 2147483647
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 int n;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
+rand() - RAND_MAX / 2;
+
 if (n < 0)
 {
-cout << "is positive";
+printf("is positive");
 }
 else if (n == 0)
 {
-cout << "is zero";
+printf("is zero");
 }
 else if (n > 0)
 {
-cout << "is negative";
+printf("is negative");
 }
 return (0);
 }
