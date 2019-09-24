94e436472ec07f86595f74ec8347c9e66abd97a0
vhalard <eagomez2387@gmail.com>
2019-09-24 22:52:27 +0000

    Add the point 4

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * main - check the code for Holberton School students.
  *
@@ -9,11 +7,13 @@ int main(void)
 {
 	int r;
 
-	r = _islower('H');
+	r = _isalpha('H');
 	_putchar(r + '0');
-	r = _islower('o');
+	r = _isalpha('o');
 	_putchar(r + '0');
-	r = _islower(108);
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
