4427b1a8e67f35c0d38ab287b10ef20b0a56a4d1
vhalard <eagomez2387@gmail.com>
2019-09-24 23:32:31 +0000

    fix the description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * add - Entry point
- *@n - variable
- *@x - variable 2
- * Return: Always 0 (Success)
+ * add - adds two integers and returns the result
+ * @a: parameters input
+ * @b: parameters input
+ * Return: adds twu integers
  */
 
 int add(int n, int x)
