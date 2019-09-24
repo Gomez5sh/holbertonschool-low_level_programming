8018402e562ed8a51e9f4c16d0b6379cd063371c
vhalard <eagomez2387@gmail.com>
2019-09-24 21:56:07 +0000

    fix the header, and add a coment

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
-#include "stdio.h"
+#include "holberton.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _islower - returns 1 if lowercase alphabet and 0 otherwise
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
  */
 
-int _islower(int c)
+int _islower(int c);
 
 {
 	if (c <= 'a' && c <= 'z')
