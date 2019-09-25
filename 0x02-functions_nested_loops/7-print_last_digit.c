338be21635c052975f464a505cce93126c3e1517
vhalard <eagomez2387@gmail.com>
2019-09-25 02:15:34 +0000

    Create the c program for the point 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - returns 1 if lowercase alphabet and 0 otherwise
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+
+int print_last_digit(int n)
+
+{
+	int x = n % 10;
+
+	if (x < 0)
+	{
+		x = x * -1;
+		_putchar(x + '0');
+
+	}
+
+	else
+	{
+		_putchar(x + '0');
+	}
+	return (x);
+}
