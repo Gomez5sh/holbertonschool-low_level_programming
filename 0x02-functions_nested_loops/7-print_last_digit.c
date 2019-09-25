3a291a359c539fba14e16a2ca598e556e14852b5
vhalard <eagomez2387@gmail.com>
2019-09-25 02:17:06 +0000

    fix the coment

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - returns 1 if lowercase alphabet and 0 otherwise
- * @c: takes in a character
+ * @n: takes in a character
  * Return: 0 for uppercase, 1 for lowercase
  */
 
