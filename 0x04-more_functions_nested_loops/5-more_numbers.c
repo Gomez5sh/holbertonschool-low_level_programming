c85f2a84aa7b07a2c4f82ff5e25f7c12ae6ed93e
vhalard <eagomez2387@gmail.com>
2019-09-26 19:56:14 +0000

    Add more more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,17 @@ void more_numbers(void)
 	int x;
 	int n;
 
-	for (x = '0'; x <= '14'; x++)
-		_putchar(x);
-	for (n = '0'; n <= '14'; n++)
-		_putchar(n);
-	_putchar('\n');
+	for (x = 0; x < 10; x++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+
+		if (n > 9)
+		{
+			_putchar((n / 10) + '0');
+		}
+		_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
