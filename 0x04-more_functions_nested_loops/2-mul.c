4501d3bb667bb5274f04376b6a86f96ce10eb774
vhalard <eagomez2387@gmail.com>
2019-09-26 16:27:38 +0000

    Time to mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @a: is a character
+ * @b: is a character
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+
+int mul(int a, int b)
+
+{
+
+	return (a * b);
+}
