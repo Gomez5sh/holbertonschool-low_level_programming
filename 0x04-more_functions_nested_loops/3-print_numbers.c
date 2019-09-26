c040a3b93233e71e2f03bd7df2f5fd5ad201f4e6
vhalard <eagomez2387@gmail.com>
2019-09-26 17:23:29 +0000

    Add the string of numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - that prints the numbers.
+ *
+ * Return: adds twu integers
+ */
+
+void print_numbers(void)
+
+{
+
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @a: is a character
+ * @b: is a character
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+
+void print_most_numbers(void)
+{
+
+	int n;
+	for (n = '0'; n <= '9'; n++)
+	{
