b64b1cfb4940e713e94a6fe48f3d66f14e808191
vhalard <eagomez2387@gmail.com>
2019-09-27 22:04:18 +0000

    Fix the point 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,27 +14,31 @@ int main(void)
 	int n;
 
 	for (n = 1; n <= 100; n++)
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
+	{
 
-		else if (n % 3 != 0)
+		if ((n % 3 != 0) && (n % 5 != 0))
 		{
-			printf("Fizz");
+			printf("%d ", n);
 		}
-		else if (n % 5 != 0)
+		else
 		{
-			printf("Buzz");
+			if (n % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (n % 5 == 0)
+			{
+				printf("Buzz");
+			}
 		}
-	if (n != 100)
-	{
-		printf(" ");
-	}
-	else
-	{
-		printf("\n");
+		if (n != 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
 	return (0);
-
 }
