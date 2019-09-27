15338d60f9ed18bf62e5ac98d4063fea479628f3
vhalard <eagomez2387@gmail.com>
2019-09-27 14:00:06 +0000

    Fix the if in the point 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,12 @@ void print_square(int size)
 {
   int l, f;
 
-  if (size != 0 && size < 0)
+  if (size < 1)
     {
       _putchar('\n');
     }
   else
-    { 
+    {
   for (l = 1; l <= size; l++)
     {
       for (f = 1; f <= size; f++)
