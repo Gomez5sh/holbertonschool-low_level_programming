114d29422d5bee2806c7ccc94827ee768b5735a3
vhalard <eagomez2387@gmail.com>
2019-09-27 20:30:44 +0000

    make the fizzbuzz point

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *FizzBuzz
+ *
+ *@n: funtion of the program
+ *
+ *Return: is 0
+ */
+
+int main ()
+{
+
+	int n;
+
+	for (n = 1; n <= 100; n++)
+		if (( n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (n % 3 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 != 0)
+		{
+			printf("Buzz");
+		}
+	if (n != 100)
+	{
+		printf(" ");
+	}
+	else
+	{
+		printf ("\n");
+	}
+	return (0);
+
+}
