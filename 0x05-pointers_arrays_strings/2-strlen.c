cdaebd679c2b5b9bedc64060b2181e5425c9c5c0
vhalard <eagomez2387@gmail.com>
2019-10-01 15:27:04 +0000

    Add the point two

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,20 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - reset
- * @n: pointer
- * Return: 0 for uppercase
+ * _strlen - count the length
+ * @s: pointer
+ * Return: len for uppercase
  */
 
 int _strlen(char *s)
 
 {
+	int len = 0;
 
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
