10424d5dcdd2c8d5619ee252216c58341e59cb6b
vhalard <eagomez2387@gmail.com>
2019-10-01 22:18:13 +0000

    Make the point 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Prints every other character of a string
+ * @str: The evaluated input
+ *
+ * Return: n value
+ */
+
+void puts_half(char *str)
+{
+	int n = 0;
+	int l;
+	int c;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
