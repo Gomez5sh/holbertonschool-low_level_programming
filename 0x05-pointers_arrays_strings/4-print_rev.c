37291f3fb830bb2f1de4e66c3639320f0d56b136
vhalard <eagomez2387@gmail.com>
2019-10-01 19:13:44 +0000

    point 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _puts - print
+ * @str: pointer
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+
+	int x;
+	int n;
+
+	n = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		n++;
+	}
+
+	for (x = n; n >= 0; n--)
+	{
+		if (x != 0)
+		{
+			_putchar(*s);
+		}
+		s--;
+	}
+
+	_putchar('\n');
+}
