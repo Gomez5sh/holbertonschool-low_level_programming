4aaeff20b1e8199492c277823b87944fedf8a4c5
vhalard <eagomez2387@gmail.com>
2019-10-08 22:37:05 +0000

    [Fix] fix fix and more fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,16 +10,16 @@
 void print_chessboard(char (*a)[8])
 {
 
-        int n, b, x;
+	int n, b, x;
 
-        n = sizeof(*a);
+	n = sizeof(*a);
 
-        for (b = 0; b < n; b++)
-        {
-                for (x = 0; x < n; x++)
-                {
-                        _putchar(a[b][x]);
-                }
-                _putchar('\n');
-        }
+	for (b = 0; b < n; b++)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(a[b][x]);
+		}
+		_putchar('\n');
+	}
 }
