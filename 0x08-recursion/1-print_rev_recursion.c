c5825275dcdb41561902345a7345763392572421
vhalard <eagomez2387@gmail.com>
2019-10-09 21:05:55 +0000

    add the point 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_rev_recursion - check the code for Holberton School students.
+ *@s : viriable
+ * Return: none
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion((s) + 1);
+		_putchar(*s);
+	}
+}
