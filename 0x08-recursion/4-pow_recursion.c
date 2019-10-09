cd89a047a73bd12e32861f3690cbfe54f7e14156
vhalard <eagomez2387@gmail.com>
2019-10-09 23:20:41 +0000

    Add the point 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - Returns the value.
+ * @x: variable 1
+ * @y: variable 2
+ *
+ *Return: -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x , y - 1));
+	}
+	else if (y < 0)
+	{
+		return (- 1);
+	}
+	else
+	{
+		return (1);
+	}
+}
