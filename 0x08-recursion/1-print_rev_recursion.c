50a78950490cace81b6c3d8a198e84e79a70c358
vhalard <eagomez2387@gmail.com>
2019-10-09 21:32:44 +0000

    Fix the point 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * _puts_rev_recursion - check the code for Holberton School students.
- *@s : viriable
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s : reverse
  * Return: none
  */
 
