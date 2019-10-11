9e0163d8e5368195bf98361549ec9a6fd83ea4de
vhalard <eagomez2387@gmail.com>
2019-10-11 20:16:12 +0000

    Add the point args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - argc my name program.
+ * @argc: varianle
+ * @argv: variable
+ * Return:  0.
+ */
+
+int main(int argc, char *argv[])
+{
+
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
