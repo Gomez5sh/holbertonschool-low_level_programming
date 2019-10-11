0e3ebc36cd07bee25e3b5ec1efffa1e8b1c6299d
vhalard <eagomez2387@gmail.com>
2019-10-11 20:07:56 +0000

    Add the point 0 argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - argc my name program.
+ * @argc: varianle
+ * @argv: variable
+ * Return:  0.
+ */
+
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+#C - argc, argv
+
+This file contais the files to the point argc and argv
