ff1953ac6b702b5f5fda7c656166b224a542f0d3
g5sh <eagomez2387@gmail.com>
2019-10-12 23:39:47 -0500

    Fix the point 4 again

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,28 +6,28 @@
  * @argc: Firts input int argument
  * @argv: Second input char argument of string
  *
- * Description: Adds positive numbers
+ * 
  * Return: 0 if it works
  */
 int main(int argc, char *argv[])
 {
-  int i = 1, j = 0, add = 0;
+int i = 1, j = 0, add = 0;
 
-  if (argc > 1)
-    {
-      while (i < argc)
-	{
-	  while (argv[i][j] != 0)
-	    {
-	      if (!isdigit(argv[i][j++]))
-		{
-		  printf("Error\n");
-		  return (1);
-		}
-	    }
-	  add += atoi(argv[i++]);
-	}
-    }
-  printf("%d\n", add);
-  return (0);
+if (argc > 1)
+{
+while (i < argc)
+{
+while (argv[i][j] != 0)
+{
+if (!isdigit(argv[i][j++]))
+{
+printf("Error\n");
+return (1);
+}
+}
+add += atoi(argv[i++]);
+}
+}
+printf("%d\n", add);
+return (0);
 }
