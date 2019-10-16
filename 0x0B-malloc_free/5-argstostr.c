e46e4673d4fb75a08f1c4247fba90446ba9cbffd
vhalard <eagomez2387@gmail.com>
2019-10-16 22:03:38 +0000

    Add the point 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _strlen - funtion 1
+ * @s: variable.
+ * Return: int value.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * argstostr - funtion 1
+ * @ac: variable.
+ * @av: variable
+ * Return: pointer to my malloc addres.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *p;
+	int b, n, i, sum;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	sum = 0;
+	for (b = 0; b < ac; b++)
+	{
+		sum = sum + _strlen(av[b]);
+	}
+
+	p = malloc((sum + ac) * sizeof(char) + 1);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	sum = 0;
+
+	for (n = 0; n < ac; n++)
+		{
+			for (i = 0; av[n][i] != '\0'; i++)
+			{
+				p[sum] = av[n][i];
+				sum++;
+			}
+			p[sum] = '\n';
+			sum++;
+		}
+	return (p);
+}
