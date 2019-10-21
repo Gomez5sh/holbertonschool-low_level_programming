c22adf83ff709b266386d621dd7d495a204a3cd4
vhalard <eagomez2387@gmail.com>
2019-10-21 23:09:50 +0000

    fix the point 2 * 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,36 +10,32 @@
 
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		if (d->name != NULL)
-		{
-			printf("Name: %s\n", d->name);
-		}
-		else
-		{
-			printf("Name: (nil)\n");
-		}
-		if (d->age > 0)
-		{
 
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)\n");
-		}
-		if (d->owner != NULL)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
-		{
-			printf("Owner: (nil)\n");
-		}
+	if (d == NULL)
+		printf("NULL");
+
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
 	}
 	else
 	{
-		printf("NULL");
+		printf("Name: (nil)\n");
+	}
+	if (d->age > 0)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
 	}
 }
