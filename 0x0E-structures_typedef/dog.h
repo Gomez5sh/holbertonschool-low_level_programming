bad3595b119eba5afe4bbb98eb03f02cf3e1e92c
vhalard <eagomez2387@gmail.com>
2019-10-21 20:20:37 +0000

    fix the dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * @name: Django.
- * @age: 3.5
- * @owner: Jay.
+ * struct struct dog - My dog info
+ * @char: name
+ * @float: age
+ * @char: owner
+ *
+ * Description: My dog information
  */
-
-
 struct dog
-
 {
 	char *name;
 	float age;
