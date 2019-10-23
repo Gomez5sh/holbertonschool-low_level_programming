2903a391585e2902d8d13287d0fa40804c904c3a
g5sh <eagomez2387@gmail.com>
2019-10-23 20:38:20 +0000

    Fix the point 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,26 +14,23 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int n;
 
 	if (size < 0)
+		return (-1);
+
+	if (array == NULL && cmp == NULL)
 	{
 		return (-1);
 	}
 	else
 	{
-		if (array == NULL && cmp == NULL)
+		for (n = 0; n < size; n++)
 		{
-			return (-1);
-		}
-		else
-		{
-			for (n = 0; n < size; n++)
+			if (cmp(array[n]))
 			{
-				if (cmp(array[n]))
-				{
-					return (n);
-				}
+				return (n);
 			}
-			return (-1);
 		}
+		return (-1);
 	}
+
 	return (-1);
 }
