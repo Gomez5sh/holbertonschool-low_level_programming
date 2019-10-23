88f0e349c4d064b462a6f7d1675cf4706d5f1514
g5sh <eagomez2387@gmail.com>
2019-10-23 22:56:38 +0000

    [add] Add the files of the funtion calc

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - funtion get
+ *
+ * @s: pointer char
+ *
+ * Return: Pointer
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op[0] != s[0] && i < 4)
+	{
+		i++;
+	}
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include  "3-calc.h"
+
+/**
+ * op_add - sum option
+ * @a: int variable
+ * @b: int variable
+ * Return: op_add
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub option
+ * @a: int variable
+ * @b: int variable
+ * Return: op_sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul option
+ * @a: int variable
+ * @b: int variable
+ * Return: op_mul
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div option
+ * @a: int variable
+ * @b: int variable
+ * Return: op_div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -mod option
+ * @a: int variable
+ * @b: int variable
+ * Return: op_mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
