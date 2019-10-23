ccce633d35bf0a5023469f692a59338b094cad5b
g5sh <eagomez2387@gmail.com>
2019-10-23 17:27:34 +0000

    [Feature] Add the point 0 Create the funtion for name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - Write a function that prints a name.
+ *
+ * @name: pointer char.
+ * @f: pointer void
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL && f == NULL)
+		return;
+	f(name);
+}
