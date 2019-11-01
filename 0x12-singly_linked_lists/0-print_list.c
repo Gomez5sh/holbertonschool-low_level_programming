07dd91d299ece9beaf87e2cd55f2fff6228ea668
g5sh <eagomez2387@gmai.com>
2019-10-31 22:43:11 -0500

    [FIX]: Fix the point 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,29 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
-
+/**
+ * print_list - print list funtion.
+ * @h: Variable.
+ * Return: node.
+ **/
 
 size_t print_list(const list_t *h)
 {
-	const list_t *temp = h;
-	size_t n;
+const list_t *temp = h;
+int n = 0;
 
-	while (temp != NULL)
-	{
-		temp->str? printf("[%u], %s\n", temp->len, temp->str):
-			printf("[%u], %s\n", temp->len, temp->str);
-		temp = temp->next;
-		n++;
-	}
-	return (n);
+while (temp != NULL)
+{
+if (temp->str == NULL)
+{
+printf("(nil)\n");
+temp = temp->next;
+n++;
+}
+printf("[%u] %s\n", temp->len, temp->str);
+temp = temp->next;
+}
+return (n);
 }
