207bbb63a808aba98e227cccf95311ed5fc69fe0
g5sh <eagomez2387@gmai.com>
2019-10-31 23:43:13 -0500

    [FIX]: Fix thepoint 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
+/**
+ * list_len - Fntion to list the len.
+ * @h: pointer.
+ * Return; node.
+ **/
 
 size_t list_len(const list_t *h)
 {
 	const list_t *temp = h;
 	size_t n;
+
 	n = 0;
 	while (temp != NULL)
 	{
 		temp = temp->next;
 		n++;
 	}
-	return(n);
+	return (n);
 }
