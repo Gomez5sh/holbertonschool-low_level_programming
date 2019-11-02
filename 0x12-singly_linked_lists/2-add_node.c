bb12097875e1ad80b00b2e9b3ce25118b52e3707
g5sh <eagomez2387@gmai.com>
2019-11-01 23:58:50 -0500

    [ADD]: ADD THE POINT 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,16 +32,16 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	new = malloc(sizeof(list_t));
-	if (!new)
-    {
-      free(new);
-		return (NULL);
-    }
-	new->str = strdup(str);
-	new->len = _strlen(str);
-	new->next = *head;
-	*head = new;
+new = malloc(sizeof(list_t));
+if (!new)
+{
+free(new);
+return (NULL);
+}
+new->str = strdup(str);
+new->len = _strlen(str);
+new->next = *head;
+*head = new;
 
 	return (new);
 }
