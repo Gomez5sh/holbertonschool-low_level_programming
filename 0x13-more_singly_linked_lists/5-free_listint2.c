4b159e6e2a900c3c950fdcb201eea1ec99af5271
g5sh <eagomez2387@gmail.com>
2019-11-05 19:31:14 +0000

    [ADD]: Add the point 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - funtion to free the memory.
+ * @head: pointer.
+ *
+ * Return: not return.
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *container, *fr;
+
+	container = *head;
+	fr = *head;
+
+	while (container->next != 0)
+	{
+		container = container->next;
+	}
+		free(fr);
+}
