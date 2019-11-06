7eb5bfb4f342fb8267b09dda12b1ecde0b6a320d
g5sh <eagomez2387@gmai.com>
2019-11-05 23:47:03 -0500

    [FIX]: Fix the point 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,25 +1,26 @@
 #include "lists.h"
 
 /**
- * pop_listint - Delete a node.
+ * get_nodeint_at_index - Add node.
  * @head: pointer.
- *
- * Return: 0.
+ * @index: unsigned variable.
+ * Return: head.
  **/
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-  unsigned int n;
+unsigned int n;
 
-  n = 0;
+n = 0;
+if (!head)
+return (NULL);
 
-  if (!head)
-    return (NULL);
-
-  while (n < index)
-    {
-      head = head->next;
-      n++;
-    }
-  return (head);
+while (n < index)
+{
+if (!head)
+return (NULL);
+head = head->next;
+n++;
+}
+return (head);
 }
