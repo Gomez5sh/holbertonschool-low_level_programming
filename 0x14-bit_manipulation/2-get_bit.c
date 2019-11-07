dc0ceeffe7c1c8f3abd3738464a34e8dfd73969e
g5sh <eagomez2387@gmail.com>
2019-11-07 22:24:50 +0000

    [ADD]: the file of point 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_bit - return the value of bits. 
+ * @n: unsigned variable.
+ * @index: unsigned variable.
+ * Return: value of bits or -1.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long h;
+	
+	h = sizeof(n) * 8;
+
+	if (index > (h - 1))
+		return (-1);
+	
+	return ((n >> index) & 1);
+}
