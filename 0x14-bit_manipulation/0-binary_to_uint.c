253f6187cdb55f2750972d0d5021dff3f58011aa
g5sh <eagomez2387@gmail.com>
2019-11-07 21:05:03 +0000

    Add the point 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Binary to decimal.
+ * @b: pointer.
+ * Return: res.
+ * */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, base;
+	unsigned int res;
+	
+	res = 0;
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+	}
+	i--;
+	for (base = 1; b[i]; i--)
+	{
+		if (b[i] == '0')
+		{
+			res += 0;
+		}
+		else
+		{
+			res += base;
+		}
+		base = base * 2;
+	}
+	return (res);
+}
