8d3d75e8fb25738f97963d6624a8177932d98fa5
g5sh <eagomez2387@gmail.com>
2019-11-07 21:30:27 -0500

    [FIX]: Fix betty style.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * get_bit - return the value of bits. 
+ * get_bit - return the value of bits.
  * @n: unsigned variable.
  * @index: unsigned variable.
  * Return: value of bits or -1.
@@ -10,11 +10,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long h;
-	
+
 	h = sizeof(n) * 8;
 
 	if (index > (h - 1))
 		return (-1);
-	
 	return ((n >> index) & 1);
 }
