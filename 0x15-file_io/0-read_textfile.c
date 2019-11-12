cf0f7916bf7b20010d490f7a80deb0a7d6e78c9b
g5sh <eagomez2387@gmail.com>
2019-11-12 21:16:23 +0000

    [FIX]: Fix the point 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,5 @@
 #include "holberton.h"
-/**
- *_strlen  - set the integer to 402
- * @s: a pointer the integer we want to set to 402
- * Return: int
- */
-int _strlen(char *s)
-{
-	int i = 0;
 
-	while (*s != '\0')
-	{
-		s++;
-		i++;
-	}
-	return (i);
-}
 /**
  * read_textfile - Reads a text file and prints.
  * @filename: pointer.
@@ -24,37 +9,34 @@ int _strlen(char *s)
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, buff, len, rd_file;
+	int fd, buff, rd_file;
 	char *size;
 
 	if (filename == NULL)
 		return (0);
-	size = malloc(sizeof(char) * letters);
-		if (!size)
-		{
-			free(size);
-			return (0);
-		}
+	size = malloc(sizeof(char) + letters);
+	if (!size)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
-		if (!fd)
-			return (0);
+	if (fd == -1)
+		return (0);
+	
 	rd_file = read(fd, size, letters);
-		if (rd_file == -1)
-		{
-			free(size);
-			close(fd);
-			close(rd_file);
-			return (0);
-		}
-		len = _strlen(size);
-		buff = write(STDOUT_FILENO, size, len);
-			if (!buff)
-			{
-				free(size);
-				return (0);
-			}
+	if (rd_file == -1)
+	{
+		free(size);
+		return (0);
+	}
+	buff = write(STDOUT_FILENO, size, rd_file);
+	if (buff == -1)
+	{
+		free(size);
+		return (0);
+	}
 	if (rd_file != buff)
 		return (0);
+	
 	close(fd);
 	free(size);
 	return (rd_file);
