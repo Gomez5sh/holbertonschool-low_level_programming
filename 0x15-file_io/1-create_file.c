954bd50f0737f5c3afc36af67ecae1096dba894d
g5sh <eagomez2387@gmail.com>
2019-11-12 21:46:25 -0500

    [Fix]: Point 1 {2}

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,40 @@
 #include "holberton.h"
 
+
 /**
- * create_file - Funtion to create a file.
- * @filename: pointer.
- * @text_content: pointer.
+ * create_file - funtion to create file.
+ * @filename: pointer
+ * @text_content: pointer
+ *
  * Return: 0.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wri, len = 0;
+	int fd, writ;
 
 	if (filename == NULL)
-		return (0);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd == -1)
-		return (0);
-	if (text_content == NULL)
-		text_content = "";
-	len = _strlen(text_content);
-
-	wri = write(fd, text_content,len);
-
-	if (wri == -1)
 		return (-1);
+
+	if (text_content == NULL)
+	{
+		return (close(fd) == -1 ? -1 : 1);
+	}
+
+	writ = write(fd, text_content, _strlen(text_content));
+
+	if (writ == -1)
+	{
+		return (-1);
+	}
 
 	close(fd);
 
-	return (-1);
+	return (1);
 }
 
 /**
@@ -37,6 +43,7 @@ int create_file(const char *filename, char *text_content)
  *
  * Return: int
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
