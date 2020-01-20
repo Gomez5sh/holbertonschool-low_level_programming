02a73a925f81f8d8d65d16220e8ef089cdd4b7f7
g5sh <eagomez2387@gmail.com>
2020-01-19 22:13:42 -0500

    [ADD]: Add point 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - find value at index key
+ * @key: key in hash table
+ * @size: size of table
+ * Return: value at index or NULL
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+if (key && size)
+return (hash_djb2(key) % size);
+else
+return (0);
+}
