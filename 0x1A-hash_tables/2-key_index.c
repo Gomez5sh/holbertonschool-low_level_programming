555925d85099a92fcf55f826bb825def1a0bf34f
g5sh <eagomez2387@gmail.com>
2020-01-19 22:17:54 -0500

    [FIX]: Fix file 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,8 +7,17 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-if (key && size)
-return (hash_djb2(key) % size);
-else
+unsigned long int hash;
+unsigned long int direct;
+
+if (!key == NULL)
 return (0);
+
+if (size == 0)
+return (0);
+
+hash = hash_djb2(key);
+direct = hash % size;
+
+return (direct);
 }
