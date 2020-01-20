5f07d2a72b668023734406d72b3c5c9ba5919b80
g5sh <eagomez2387@gmail.com>
2020-01-19 22:49:05 -0500

    [ADD]: Add header and point 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get values in hash
+ * @ht: hashtable
+ * @key: key
+ * Return: Pointer.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int i = key_index((const unsigned char *)key, ht->size);
+hash_node_t *new_node = ht->array[i];
+
+if (!ht || !key)
+return (NULL);
+
+for (new_node = ht->array[i]; new_node != NULL;
+new_node = new_node->next)
+{
+if (strcmp(new_node->key, key) == 0)
+return (new_node->value);
+}
+
+return (NULL);
+}
