1d592ab34a59f8c85c7338bbc30182545715c9af
g5sh <eagomez2387@gmail.com>
2020-01-19 22:35:38 -0500

    [ADD]: Add header

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - check the code for Holberton School students.
+ * @ht: hash table
+ * @key: key
+ * @value: value for table
+ *
+ * Return: EXIT_SUCCESS.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+hash_node_t *new_node = malloc(sizeof(hash_node_t));
+hash_node_t *temp;
+unsigned long int hashnum = key_index((unsigned char *)key, ht->size);
+
+if (!key || !ht || strlen(key) == 0 || !value || !(new_node))
+{
+free(new_node);
+return (0);
+}
+
+for (temp = ht->array[hashnum]; temp; temp = temp->next)
+{
+if (strcmp(temp->key, key) == 0)
+{
+free(temp->value);
+temp->value = strdup(value);
+if (temp->value == NULL)
+{
+free(new_node);
+return (0);
+}
+return (1);
+
+}
+
+}
+
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = ht->array[hashnum];
+ht->array[hashnum] = new_node;
+
+return (1);
+}
