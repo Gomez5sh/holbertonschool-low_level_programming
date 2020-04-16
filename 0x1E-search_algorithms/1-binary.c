2bd6d9a2bf77f097f010b894de19f338f491396c
g5sh <eagomez2387@gmail.com>
2020-04-16 14:20:06 -0500

    [ADD]POINT 1: Add point 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+/**
+ * binary_search - binary search algo.
+ *
+ * @array: pointer to head.
+ * @size: size of the array
+ * @value: value to look for inside array
+ * Return: value on success, -1 on failure
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t mid;
+	size_t i;
+
+	if (!array)
+		return (-1);
+
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+			printf("%d, ", array[i]);
+		printf("%d", array[i]);
+		printf("\n");
+
+		mid = low + (high - low) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] > value)
+			high = mid - 1;
+
+		if (array[mid] < value)
+			low = mid + 1;
+	}
+	return (-1);
+}
