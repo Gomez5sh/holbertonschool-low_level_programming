f3716a821ed2cd51765e261e13864607e40f662e
g5sh <eagomez2387@gmail.com>
2020-04-16 13:31:03 -0500

    [ADD]POINT 0: Add point 0 and header

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search algo.
+ * @array: pointer.
+ * @size: array size
+ * @value: value.
+ *
+ * Return: value on success, -1 on failure
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t n;
+
+	if (array)
+	{
+		for (n = 0; n < size; n++)
+		{
+			printf("Value checked array[%ld] = [%d]\n",
+			       n, array[n]);
+			if (array[1] == value)
+				return;
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef __SEARCH_ALGOS__
+#define __SEARCH_ALGOS__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#emdif
